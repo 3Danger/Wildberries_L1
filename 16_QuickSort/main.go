15d6bdf334a534ad5f30a8076ffdcbbd716840b9
csamuro <3danger@mail,ru>
2022-04-30 04:09:51 +0300

    16 done

---
+++ 16_QuickSort/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/*
+	Реализовать быструю сортировку массива
+	(quicksort) встроенными методами языка.
+*/
+
+func main() {
+	arr := []int{3, 2, 6, 10, 3, 8, 7, 3, 1, 0, 5}
+	fmt.Println("Before:", arr)
+	QuickSort(arr)
+	fmt.Println("After: ", arr)
+}
+
+func QuickSort(arr []int) {
+	// Берем некий индекс в качестве опорной точки
+	pivot := len(arr) - 1
+	// Если мы имеем меньше 2‑х элементов, то выходим из рекурсии
+	if pivot < 1 {
+		return
+	}
+	// left будет в качестве стартового индекса
+	left := 0
+	// Все что меньше pivot будем класть в left
+	// и переходить к следующему индексу, и так не доходя до pivot
+	for i, _ := range arr[:pivot] {
+		if arr[i] < arr[pivot] {
+			arr[left], arr[i] = arr[i], arr[left]
+			left++
+		}
+	}
+	// перемещаем pivot на свое законное место на следующую ячейку от left
+	arr[left], arr[pivot] = arr[pivot], arr[left]
+	// Разделяем на рекурсию, на левую и правую часть, не трогая часть pivot
+	QuickSort(arr[left+1:])
+	QuickSort(arr[:left])
+}
