f6e27c96a4b739f81fcfd0fb55c5ceb1715f379c
csamuro <3danger@mail,ru>
2022-04-30 15:56:17 +0300

    22 done

---
+++ 22_Math_Operations_Big_Num/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+/*
+	Разработать программу, которая перемножает,
+	делит, складывает, вычитает две числовых переменных a,b,
+	значение которых > 2^20.
+*/
+
+func main() {
+	bigInteger := NewBigInteger(922337203685477580, 302331203635477500)
+	fmt.Println(bigInteger.Add())
+	fmt.Println(bigInteger.Sub())
+	fmt.Println(bigInteger.Mul())
+	fmt.Println(bigInteger.Div())
+}
+
+type BigInteger struct {
+	a, b *big.Int
+}
+
+func (b *BigInteger) Add() *big.Int {
+	return new(big.Int).Add(b.a, b.b)
+}
+
+func (b *BigInteger) Sub() *big.Int {
+	return new(big.Int).Sub(b.a, b.b)
+}
+
+func (b *BigInteger) Mul() *big.Int {
+	return new(big.Int).Mul(b.a, b.b)
+}
+
+func (b *BigInteger) Div() *big.Int {
+	return new(big.Int).Quo(b.a, b.b)
+}
+
+func NewBigInteger(a, b int64) *BigInteger {
+	floats := &BigInteger{new(big.Int), new(big.Int)}
+	floats.a.SetInt64(a)
+	floats.b.SetInt64(b)
+	return floats
+}
