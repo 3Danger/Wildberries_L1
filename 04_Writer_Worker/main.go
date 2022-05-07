f8f56c6417c0348780e759527d88227394df208b
csamuro <3danger@mail,ru>
2022-05-07 12:41:52 +0300

    review 04 fix, forgotten sig-handler ctrl+c now added

--- 04_Writer_Worker/main.go
+++ 04_Writer_Worker/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
 )
 
@@ -12,25 +16,49 @@ import (
 	Реализовать набор из N воркеров, которые читают произвольные
 	данные из канала и выводят в stdout.
 	Необходима возможность выбора количества воркеров при старте.
+
+	Программа должна завершаться по нажатию Ctrl+C.
+	Выбрать и обосновать способ завершения работы всех воркеров.
 */
 
 func main() {
 	c := make(chan int)
-	defer close(c)
+
 	N := flag.Int("n", 3, "number of workers")
 	flag.Parse()
 
+	wg := sync.WaitGroup{}
+	wg.Add(*N)
+	notifyContext, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 	for i := 0; i < *N; i++ {
-		go func(id int, c chan int) {
-			for data := range c {
-				time.Sleep(time.Second)
-				fmt.Println("Worker id:", id, ", data:", data)
-			}
-		}(i, c)
+		go Worker(&wg, i, c)
 	}
+	Producer(notifyContext, &wg, c)
+}
 
+func Producer(ctx context.Context, wg *sync.WaitGroup, ch chan<- int) {
+loop:
 	for {
-		data := rand.Int()
-		c <- data
+		select {
+		case <-ctx.Done():
+			fmt.Println("\tSignal ctrl+c handled")
+			close(ch)
+			wg.Wait()
+			fmt.Println("\tMain thread are closed")
+			break loop
+		default:
+			data := rand.Int()
+			ch <- data
+		}
 	}
+}
+
+func Worker(wg *sync.WaitGroup, id int, c chan int) {
+	defer wg.Done()
+	for data := range c {
+		fmt.Println("Worker id:", id, ", data:", data)
+		time.Sleep(time.Millisecond * 300)
+	}
+	fmt.Printf("Worker ID: %d, closed\n", id)
 }
